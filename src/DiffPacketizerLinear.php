181efff0512b2c3d2f6c32d29272db7718a48928
dexen deVries <dexen.devries@gmail.com>
2020-08-16 20:38:37 +0200

    move code to class

--- src/DiffPacketizerLinear.php
+++ src/DiffPacketizerLinear.php
@@ -21,6 +21,32 @@ class DiffPacketizerLinear implements DiffPacketizer
 
 	function getDiffPackets() : array
 	{
-		return diff_packets_linear($this->lines_a, $this->lines_b);
+		$packets = [];
+
+		$lines_a = $this->lines_a;
+		$lines_b = $this->lines_b;
+		$packet = [ 'aa' => [], 'bb' => [] ];
+
+		while ($lines_a && $lines_b) {
+			$aa = array_shift($lines_a);
+			$bb = array_shift($lines_b);
+
+			if ($aa[1] === $bb[1]) {
+				if ($packet['aa'] || $packet['bb']) {
+					$packets[] = $packet;
+					$packet = [ 'aa' => [], 'bb' => [] ]; } }
+			else {
+				$packet['aa'][] = $aa;
+				$packet['bb'][] = $bb; } }
+
+		while ($lines_a)
+			$packet['aa'][] = array_shift($lines_a);
+		while ($lines_b)
+			$packet['bb'][] = array_shift($lines_b);
+
+		if ($packet['aa'] || $packet['bb'])
+			$packets[] = $packet;
+
+		return $packets;
 	}
 }
